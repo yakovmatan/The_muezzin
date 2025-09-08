14c6662d6ee501ca4bd102209749d8385ebe0100
yakov matan <yakovmatan@gmail.com>
2025-09-08 10:00:11 +0300

    improve the unique identifier

--- processing/src/unique_identifier.py
+++ processing/src/unique_identifier.py
@@ -1,7 +1,8 @@
+import hashlib
 #Function to generate a unique identifier
 def get_unique_identifier(documents_json: dict, num_of_doc: str):
     unique_identifier = num_of_doc #Initialization with a unique number
-    for field in documents_json:
-        unique_identifier += documents_json[field][0] #First letter in each field
+    unique_identifier += ''.join([str(val) for val in documents_json.values()])
+    return hashlib.md5(unique_identifier.encode('utf-8')).hexdigest()
 
-    return unique_identifier
+

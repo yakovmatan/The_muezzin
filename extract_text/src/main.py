ada91ee56c6f33d86aad31ea1b8c3a94e0a16629
yakov matan <yakovmatan@gmail.com>
2025-09-09 10:01:43 +0300

    create main

---
+++ extract_text/src/main.py
@@ -0,0 +1,12 @@
+from extract_text.src.consumer import Consumer
+from extract_text.src.config import *
+
+consumer = Consumer(TOPIC_SUB, index_name=INDEX_NAME)
+
+
+def main():
+    consumer.consume_messages()
+
+
+if __name__ == '__main__':
+    main()

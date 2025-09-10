b7356ee01854c5e92c6ecbce51916dd363cc1ec2
yakov matan <yakovmatan@gmail.com>
2025-09-10 12:07:29 +0300

    add comments

--- extract_text/src/decoder.py
+++ extract_text/src/decoder.py
@@ -5,6 +5,7 @@ class Decoder:
     def __init__(self, code):
         self.code = code
 
+    # Base64 encryption converter
     def decoded_from_base64(self):
         base64_bytes = self.code.encode('ascii')
         decoded_bytes = base64.b64decode(base64_bytes)

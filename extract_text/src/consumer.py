c3ccb9b480086a33735c4a1187ee9de216fd2832
yakov matan <yakovmatan@gmail.com>
2025-09-10 09:30:33 +0300

    change file location

--- extract_text/src/consumer.py
+++ extract_text/src/consumer.py
@@ -2,28 +2,30 @@ from configurations.elastic_configuration import ElasticConn
 from configurations.kafka_configuration import consumer, produce, send_event
 from configurations.mongodb_configuration import DbConnection
 from extract_text.src.convert_bytes import ConvertBytes
+from extract_text.src.decoder import Decoder
+from extract_text.src.enricher import Enricher
 from extract_text.src.text_extraction import TextExtraction
 from logger.logger_to_elasic import Logger
 from dals.dal_elastic import DalElastic
 from dals.dal_mongo import DalMongo
+from extract_text.src.fiels import *
 
 logger = Logger.get_logger()
 
 
 class Consumer:
 
-    def __init__(self, *topics_sub, topic_pub, index_name: str, new_field='text'):
-        self.new_field = new_field
+    def __init__(self, *topics_sub, index_name: str):
         self.index_name = index_name
         self.topic_sub = topics_sub
-        self.topic_pub = topic_pub
         self.events = consumer(*topics_sub)
-        self.producer = produce()
         self.mongo_conn = DbConnection()
         self.elastic_conn = ElasticConn().get_es()
         self.dal_mongo = DalMongo(self.mongo_conn)
         self.dal_elastic = DalElastic(self.elastic_conn)
         self.text_extract = TextExtraction()
+        self.list_hostile = Decoder(HOSTILE).decoded_from_base64().split(',')
+        self.list_less_hostile = Decoder(LESS_HOSTILE).decoded_from_base64().split(',')
 
     @staticmethod
     def extract_id_from_message(document, field='id'):
@@ -38,6 +40,17 @@ class Consumer:
         text = self.text_extract.extract_text_from_a_file(file_audio)
         return text
 
+    def __fields_and_value_to_add(self, text):
+        fields_and_value = {'text': text}
+        score, word_count = Enricher.risk_score(text, self.list_hostile, self.list_less_hostile)
+        bds_percent = Enricher.danger_percentages(len(text), score, word_count)
+        fields_and_value['bds_percent'] = bds_percent
+        is_bds = Enricher.is_bds(bds_percent)
+        fields_and_value['is_bds'] = is_bds
+        bds_threat_level = Enricher.risk_level(bds_percent)
+        fields_and_value['bds_threat_level'] = bds_threat_level
+        return fields_and_value
+
 
     def consume_messages(self):
         logger.info(f"starting to consume from: {self.topic_sub}")
@@ -47,6 +60,4 @@ class Consumer:
             # Extract text
             text = self.__extract_text(unique_id)
             # Update in Elasticsearch
-            self.dal_elastic.add_field(self.index_name, unique_id, text, self.new_field)
-            # sending to kafka in
-            send_event(self.producer, self.topic_pub, {'id': unique_id})
+            self.dal_elastic.add_fields(self.index_name, unique_id, **self.__fields_and_value_to_add(text))
--- extract_text/src/main.py
+++ extract_text/src/main.py
@@ -1,7 +1,7 @@
 from extract_text.src.consumer import Consumer
 from extract_text.src.config import *
 
-consumer = Consumer(TOPIC_SUB, index_name=INDEX_NAME, topic_pub=TOPIC_PUB)
+consumer = Consumer(TOPIC_SUB, index_name=INDEX_NAME)
 
 
 def main():
